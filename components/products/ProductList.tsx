f68e3d08f292d4b6cf846136ed433bdb73e8d123
Tareq <tareqnmd@gmail.com>
2024-07-24 23:27:20 +0600

    card update

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -2,17 +2,29 @@
 import { useGetProducts } from '@/hooks/useGetProducts';
 import { ProductListType } from '@/types/product';
 import ProductCard from './ProductCard';
+import ProductSkeleton from './ProductSkeleton';
 
 const ProductList = ({ products }: { products: ProductListType }) => {
 	const productList = useGetProducts(products);
 	return (
 		<div className="product-list grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-			{productList?.map((product, index) => (
-				<ProductCard
-					product={product}
-					key={index}
-				/>
-			))}
+			{productList?.length > 0 ? (
+				productList?.map((product, index) => (
+					<ProductCard
+						product={product}
+						key={index}
+					/>
+				))
+			) : (
+				<>
+					<ProductSkeleton />
+					<ProductSkeleton />
+					<ProductSkeleton />
+					<ProductSkeleton />
+					<ProductSkeleton />
+					<ProductSkeleton />
+				</>
+			)}
 		</div>
 	);
 };
