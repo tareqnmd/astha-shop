e42d3ae15e2a88da6885336811634127f04635bf
Tareq <tareqnmd@gmail.com>
2024-07-24 22:59:02 +0600

    detail image

---
+++ components/products/ProductImage.tsx
@@ -0,0 +1,22 @@
+'use client';
+import Image from 'next/image';
+
+const ProductImage = ({ src, alt }: { alt: string; src: string }) => {
+	return (
+		<Image
+			src={src}
+			width="0"
+			height="0"
+			sizes="100vw"
+			style={{
+				width: '100%',
+				height: '100%',
+			}}
+			className="object-contain transition-opacity opacity-0 duration-[0.5s]"
+			onLoadingComplete={(img) => img.classList.remove('opacity-0')}
+			alt={alt}
+		/>
+	);
+};
+
+export default ProductImage;
--- components/products/SingleProduct.tsx
+++ components/products/SingleProduct.tsx
@@ -1,18 +1,17 @@
 import { ProductType } from '@/types/product';
-import Image from 'next/image';
+import ProductImage from './ProductImage';
 import SingleProductDetails from './SingleProductDetails';
 
 const SingleProduct = ({ product }: { product: ProductType }) => {
 	const { productName, productImage } = product;
 	return (
 		<div className="flex flex-col gap-4">
-			<Image
-				src={productImage}
-				alt={productName}
-				width={200}
-				height={200}
-				className="w-full h-[300px] object-contain rounded bg-secondary"
-			/>
+			<div className="h-[50dvh]">
+				<ProductImage
+					alt={productName}
+					src={productImage}
+				/>
+			</div>
 			<SingleProductDetails product={product} />
 		</div>
 	);
