194a46a52b1a65ced39826a48b5959fc6517cf30
Tareq <tareqnmd@gmail.com>
2024-07-25 14:31:42 +0600

    image and text updated at product details

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,5 +1,5 @@
 import { CartProductType } from '@/types/product';
-import Image from 'next/image';
+import ProductImage from '../products/ProductImage';
 import CartItemRemove from './CartItemRemove';
 
 const CartItem = ({ cartItem }: { cartItem: CartProductType }) => {
@@ -8,22 +8,22 @@ const CartItem = ({ cartItem }: { cartItem: CartProductType }) => {
 
 	return (
 		<div className="flex items-center gap-2 bg-secondary p-2 rounded-lg shadow relative hover:shadow-lg">
-			<Image
-				src={productImage}
-				alt={productName}
-				width={50}
-				height={50}
-				className="w-[50px] h-[50px] object-cover rounded-lg shrink-0"
-			/>
+			<div className="w-[50px] h-[50px] overflow-hidden shrink-0 ">
+				<ProductImage
+					src={productImage}
+					alt={productName}
+					extraClass="rounded-lg object-cover rounded-lg hover:scale-100"
+				/>
+			</div>
 			<div className="flex flex-col flex-grow word-break-word">
-				<span className="text-[16px] leading-[16px]">{productName}</span>
-				<div className="flex items-center gap-1">
+				<span className="text-[14px] leading-[16px]">{productName}</span>
+				<div className="text-[14px] flex items-center gap-1">
 					<span>{addedQuantity}</span>
 					<span>x</span>
 					<span>{price}</span>
 				</div>
 			</div>
-			<strong className="self-end">{addedQuantity * price}</strong>
+			<strong className="self-end text-[14px]">{addedQuantity * price}</strong>
 			<CartItemRemove
 				addedQuantity={addedQuantity}
 				productId={productId}
--- components/products/ProductImage.tsx
+++ components/products/ProductImage.tsx
@@ -1,7 +1,16 @@
 'use client';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
-const ProductImage = ({ src, alt }: { alt: string; src: string }) => {
+const ProductImage = ({
+	src,
+	alt,
+	extraClass,
+}: {
+	alt: string;
+	src: string;
+	extraClass?: string;
+}) => {
 	return (
 		<Image
 			src={src}
@@ -12,7 +21,10 @@ const ProductImage = ({ src, alt }: { alt: string; src: string }) => {
 				width: '100%',
 				height: '100%',
 			}}
-			className="object-contain transition-all opacity-0 duration-[0.5s] hover:scale-110"
+			className={cn(
+				'object-contain transition-all opacity-0 duration-[0.5s] hover:scale-110',
+				extraClass
+			)}
 			onLoadingComplete={(img) => img.classList.remove('opacity-0')}
 			alt={alt}
 		/>
